92e2e847dcef247b16a9ef01552496216fe3106f
Jezuit_dev <122024710+Jezuit@users.noreply.github.com>
2025-10-07 12:51:19 +0200

    Update loops.py

--- Codes/Python/loops.py
+++ Codes/Python/loops.py
@@ -1,1 +1,37 @@
+#This code repeats "hello world" 10 times
+"""
+for i in range(10):
+    print("hello world")
+"""
 
+#This code counts to 9
+"""
+for i in range(10):
+    print(i)
+"""
+
+#This code counts from 1 to 9
+"""
+for i in range(1,10):
+    print(i)
+"""
+
+#this code counts up to 10 using only odd numbers
+"""
+for i in range(1,10,2):
+    print(i)
+"""
+
+#This code repeats "hello world" endlessly
+"""
+while 1 > 0:
+    print("hello world")
+"""
+
+#This code counts to 9 but stops on before number is equal to 7 (In this case, the "==" command equates the number to the variable, rather than entering it as in the case of the "=" command))
+"""
+for i in range(10):
+    if i == 7:
+        break
+    print(i)
+"""
